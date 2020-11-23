7210e30a78dd417e014296f9afcc13203b8d2085
Van Darrell Ponce <riversideriver@yahoo.com>
2020-11-23 23:06:47 +0800

    temporary changes

--- public/script.js
+++ public/script.js
@@ -1,14 +1,44 @@
 const socket = io('/')
 const peer = new Peer(undefined, {
-	path: '/peerjs',
+	path: '/',
 	host: '/',
 	port: '5001',
 })
+let myVideoStream
 
 // DOM Elements
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const videoGrid = document.getElementById('video-grid')
+
+// PEER AND SOCKET CONNECTIONS
+peer.on('open', (userId) => {
+	console.log(userId)
+	socket.emit('join-room', roomId, userId)
+})
+
+navigator.mediaDevices
+	.getUserMedia({
+		video: true,
+		audio: true,
+	})
+	.then((stream) => {
+		myVideoStream = stream
+		addVideoStream(myVideo, stream)
+
+		peer.on('call', (call) => {
+			call.answer(stream)
+			const video = document.createElement('video')
+			call.on('stream', (userVideoStream) => {
+				console.log('on stream 2')
+				addVideoStream(video, userVideoStream)
+			})
+		})
+
+		socket.on('user-connected', (userId) => {
+			connectToNewUser(userId, stream)
+		})
+	})
 
 // FUNCTIONS
 const addVideoStream = (video, stream) => {
@@ -19,25 +49,14 @@ const addVideoStream = (video, stream) => {
 	videoGrid.append(video)
 }
 
-const connectToNewUser = (userId) => {
-	console.log(userId)
-}
-
-// PEER AND SOCKET CONNECTIONS
-peer.on('open', (userId) => {
-	socket.emit('join-room', roomId, userId)
-})
-socket.on('user-connected', (userId) => {
-	connectToNewUser(userId)
-})
-
-const main = async () => {
-	const stream = await navigator.mediaDevices.getUserMedia({
-		video: true,
-		audio: true,
+const connectToNewUser = (userId, stream) => {
+	const call = peer.call(userId, stream)
+	console.log(call)
+	const video = document.createElement('video')
+	call.on('stream', (userVideoStream) => {
+		console.log('on stream 1')
+		addVideoStream(video, userVideoStream)
 	})
 
-	addVideoStream(myVideo, stream)
+	console.log('hey')
 }
-
-main()
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import path from 'path'
-import userRoutes from './routes/userRoutes.js'
-import connectDB from './config/db.js'
-import errorHandler from './middlewares/errorHandler.js'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
-import http from 'http'
-import { v4 as uuidv4 } from 'uuid'
-import { Server } from 'socket.io'
-import { ExpressPeerServer } from 'peer'
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const path = require('path')
+/* const userRoutes = require('./routes/userRoutes.js')
+const errorHandler = require('./middlewares/errorHandler.js')
+const connectDB = require('./config/db.js') */
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const http = require('http')
+const { v4: uuidv4 } = require('uuid')
+
+/* const { ExpressPeerServer } = require('peer') */
+
+const { PeerServer } = require('peer')
+const peerServer = PeerServer({ debug: true })
 
 // APP CONFIG
 dotenv.config()
@@ -20,15 +23,17 @@ app.use(express.json())
 app.use(cors())
 
 const server = http.Server(app)
-const io = new Server(server)
+const io = require('socket.io')(server)
 
-const peerServer = ExpressPeerServer(server, { debug: true })
+/* const peerServer = ExpressPeerServer(server, {
+	debug: true,
+}) */
 app.use('/peerjs', peerServer)
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 // DATBASE CONNECTION
-connectDB()
+/* connectDB() */
 
 // SOCKET CONNECTION
 io.on('connection', (socket) => {
@@ -45,25 +50,24 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 // ROUTES
-app.get('/', (req, res) => {
-	res.redirect(`${uuidv4()}`)
+app.get('/', (_, res) => {
+	res.redirect(`/${uuidv4()}`)
 })
 
 app.get('/:roomId', (req, res) => {
 	res.render('room', { roomId: req.params.roomId })
 })
 
-app.use('/api/users', userRoutes)
+/* app.use('/api/users', userRoutes)
 
-const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.all('*', (req, res) =>
 	res.status(404).send({ message: `Not found - ${req.originalUrl}` })
-)
+) */
 
 // ERROR HANDLER
-app.use(errorHandler)
+/* app.use(errorHandler) */
 
 // PORT CONFIG
 const port = process.env.PORT || 5005
